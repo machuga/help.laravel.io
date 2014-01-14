4b56abc41cad53339cc908d32d521268f84c6a4e
Matt Stauffer <mstauffer@gmail.com>
2014-01-13 21:38:19 -0500

    Fix local environment to reference database correctly, and to detect localhost sans :8000.

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -47,7 +47,7 @@ return array(
 	'connections' => array(
 		'sqlite' => array(
 			'driver'   => 'sqlite',
-			'database' => __DIR__.'/../database/production.sqlite',
+			'database' => __DIR__.'/../../database/production.sqlite',
 			'prefix'   => '',
 		),
 	),
